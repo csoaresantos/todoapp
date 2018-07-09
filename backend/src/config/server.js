8892a34aa0ecfb4568806d3893c50e897520c2ae
Charles <charlessoaresdossantos@gmail.com>
2018-07-09 18:10:59 -0300

    criação do cors pra filtrar os response

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,12 +1,15 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const allowCors = require('./cors')
 const server = express()
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
-
+server.use(allowCors)
 const port = 3003
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}`)
 })
+
+module.exports = server
