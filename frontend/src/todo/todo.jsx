8c0474f23504037cd715bc6978ab11b54a7e2c5b
Charles <charlessoaresdossantos@gmail.com>
2018-07-23 21:18:42 -0300

    criação dos métodos de manipulação do formulário

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,25 @@ import TodoForm from '../todo/todoForm'
 import TodoList from '../todo/todoList'
 
 export default class Todo extends Component {
+    constructor(props) {
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.state = { description: '', list: [] }
+    }
+    handleAdd() {
+        console.log('Funcionou!!!')
+    }
+    handleChange(e) {
+        const attrs = this.state;
+        attrs.description = e.target.value
+        this.setState(attrs)
+    }
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' />
-                <TodoForm />
+                <TodoForm handleAdd={this.handleAdd} description={this.state.description} handleChange={this.handleChange} />
                 <TodoList />
             </div>
         )
